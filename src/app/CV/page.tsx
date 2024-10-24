74431fae1315d45d276698d92044a8ed79463e0d
Shecraa <gustaveronic@gmail.com>
2024-10-24 18:31:40 +0700

    added CV v1

--- src/app/CV/page.tsx
+++ src/app/CV/page.tsx
@@ -26,7 +26,7 @@ const page = () => {
     <div className='w-[100%] h-[52vh] rounded-md flex flex-col items-center pt-4 justify-center border-2 border-greyBox mb-4'>
     <div className="mb-6 p-0 flex flex-col">
         <h3 className="text-3xl font-bold mb-2 bg-gradient-to-b from-grey to-contrast text-transparent bg-clip-text">Hey There!</h3>
-        <h3 className="text-xl  font-semibold mb-4 bg-gradient-to-b from-grey to-contrast text-transparent bg-clip-text"> I'm currently</h3>
+        <h3 className="text-xl  font-semibold mb-4 bg-gradient-to-b from-grey to-contrast text-transparent bg-clip-text"> I&apos;m currently</h3>
         <div className="bg-green-50 border border-green-200 rounded-lg p-4">
           <p className="text-green-800">
             🎓 Computer Science Student at BINUS
